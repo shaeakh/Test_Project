724428832ada4d39f4f0f12156961daff8ffe2c9
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-10-29 01:38:18 +0600

    .

--- lib/pages/Prescription_analysis.dart
+++ lib/pages/Prescription_analysis.dart
@@ -1,56 +1,30 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../widgets/PA/PrescriptionAnalysis.dart';
 
 class Prescription_Analysis extends StatefulWidget {
-  Prescription_Analysis({super.key});
+  final String responseData;
+
+  Prescription_Analysis({required this.responseData}); // Constructor
 
   @override
   State<Prescription_Analysis> createState() => _Prescription_AnalysisState();
 }
 
 class _Prescription_AnalysisState extends State<Prescription_Analysis> {
-  String isNeedEmptyStomach = '';
-  int age = 23;
+  late Future<PrescriptionAnalysis> futurePrescriptionAnalysis; // Declare future variable
 
-  late Future<PrescriptionAnalysis> futurePrescriptionAnalysis;
-
-  // Fetching data from the JSON file
+  // Fetching data from the response JSON
   Future<PrescriptionAnalysis> fetchPrescriptionAnalysis() async {
-    final url = Uri.parse(
-        'https://raw.githubusercontent.com/shaeakh/code-share/main/data.json');
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-      return PrescriptionAnalysis.fromJson(jsonResponse);
-    } else {
-      throw Exception('Failed to load prescription analysis');
-    }
-  }
-
-  String convertExpression(String expression) {
-    // Create a mapping for the values
-    Map<String, String> map = {
-      'no': '1',
-      'yes': '2',
-      'x': '0',
-    };
-
-    // Split the input expression by '-'
-    List<String> parts = expression.split('-');
-
-    // Map each part to its corresponding value and join them
-    String result = parts.map((part) => map[part] ?? part).join('');
-
-    return result;
+    // Parse the JSON from the responseData
+    final Map<String, dynamic> jsonResponse = jsonDecode(widget.responseData);
+    return PrescriptionAnalysis.fromJson(jsonResponse);
   }
 
   @override
   void initState() {
     super.initState();
-
     // Initialize the future data
     futurePrescriptionAnalysis = fetchPrescriptionAnalysis();
 
@@ -60,9 +34,8 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text("Do you want to set alarm for all these medicines?"),
+            title: Text("Do you want to set an alarm for all these medicines?"),
             actions: [
-
               ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).pop(); // Close the dialog
@@ -71,7 +44,7 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.greenAccent, // Set the background color
                 ),
-                child: Text("Yes",style: TextStyle(color: Colors.white),),
+                child: Text("Yes", style: TextStyle(color: Colors.white)),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -81,7 +54,7 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
                   Navigator.of(context).pop(); // Close the dialog
                   // Handle 'No' action
                 },
-                child: Text("No",style: TextStyle(color: Colors.white),),
+                child: Text("No", style: TextStyle(color: Colors.white)),
               ),
             ],
           );
@@ -90,8 +63,7 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
     });
   }
 
-
-
+  @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3, // Number of tabs
@@ -136,15 +108,16 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
                         height: 100,
                       ),
                       Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Name : ${prescriptionData.patientName}"),
-                              Text("Age : ${prescriptionData.age}")
-                            ],
-                          )),
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name : ${prescriptionData.patientName}"),
+                            Text("Age : ${prescriptionData.age}"),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                   Expanded(
@@ -155,7 +128,7 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
                           itemCount: prescriptionData.data.length,
                           itemBuilder: (context, index) {
                             final medicine = prescriptionData.data[index];
-                            isNeedEmptyStomach = convertExpression(medicine.takingTime);
+                            String isNeedEmptyStomach = convertExpression(medicine.takingTime);
                             return Card(
                               child: Container(
                                 padding: EdgeInsets.all(10),
@@ -163,53 +136,32 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
                                   mainAxisAlignment: MainAxisAlignment.start,
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-
                                     Text(
-                                        "Medicine ${index < 9 ? '0${index + 1}' : '${index + 1}'}: ${medicine.medicineName}"),
+                                      "Medicine ${index < 9 ? '0${index + 1}' : '${index + 1}'}: ${medicine.medicineName}",
+                                    ),
                                     Row(
                                       children: <Widget>[
                                         Text("Taking Time: "),
-                                        Padding(
-                                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-                                          child: Container(
-                                            width: 20.0,
-                                            height: 20.0,
-                                            decoration: BoxDecoration(
-                                              color: prescriptionData.data[index].takingTime[0] =='1' ? Color(0xFF18C299) : Color(0xFFCADADA),
-                                              shape: BoxShape.circle,
+                                        ...List.generate(3, (i) {
+                                          return Padding(
+                                            padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                                            child: Container(
+                                              width: 20.0,
+                                              height: 20.0,
+                                              decoration: BoxDecoration(
+                                                color: medicine.takingTime[i * 2] == '1'
+                                                    ? Color(0xFF18C299)
+                                                    : Color(0xFFCADADA),
+                                                shape: BoxShape.circle,
+                                              ),
                                             ),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-                                          child: Container(
-                                            width: 20.0,
-                                            height: 20.0,
-                                            decoration: BoxDecoration(
-                                              color: prescriptionData.data[index].takingTime[2] =='1' ? Color(0xFF18C299) : Color(0xFFCADADA),
-                                              shape: BoxShape.circle,
-                                            ),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-                                          child: Container(
-                                            width: 20.0,
-                                            height: 20.0,
-                                            decoration: BoxDecoration(
-                                              color: prescriptionData.data[index].takingTime[4] =='1' ? Color(0xFF18C299) : Color(0xFFCADADA),
-                                              shape: BoxShape.circle,
-                                            ),
-                                          ),
-                                        )
+                                          );
+                                        }),
                                       ],
                                     ),
-                                    Text(
-                                        "Empty Stomach Required: ${medicine.isNeedEmptyStomach}"),
+                                    Text("Empty Stomach Required: $isNeedEmptyStomach"),
                                     Text("Usage: ${medicine.medicineUsage}"),
                                     Text("Side Effect: ${medicine.sideEffect}"),
-
-
                                   ],
                                 ),
                               ),
@@ -235,8 +187,7 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
                           itemBuilder: (context, index) {
                             return Card(
                               child: ListTile(
-                                title: Text(
-                                    "Test: ${prescriptionData.test[index]}"),
+                                title: Text("Test: ${prescriptionData.test[index]}"),
                               ),
                             );
                           },
@@ -253,5 +204,22 @@ class _Prescription_AnalysisState extends State<Prescription_Analysis> {
         ),
       ),
     );
+  }
+
+  String convertExpression(String expression) {
+    // Create a mapping for the values
+    Map<String, String> map = {
+      'no': '1',
+      'yes': '2',
+      'x': '0',
+    };
+
+    // Split the input expression by '-'
+    List<String> parts = expression.split('-');
+
+    // Map each part to its corresponding value and join them
+    String result = parts.map((part) => map[part] ?? part).join('');
+
+    return result;
   }
 }
--- lib/pages/temp_pa.dart
+++ lib/pages/temp_pa.dart
@@ -8,6 +8,10 @@ import 'package:poject/pages/Prescription_analysis.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../main.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+import 'package:mime/mime.dart';
+import 'package:http_parser/http_parser.dart';
 
 class Temp_Pa extends StatefulWidget {
   Temp_Pa({super.key});
@@ -18,6 +22,7 @@ class Temp_Pa extends StatefulWidget {
 
 class _Temp_PaState extends State<Temp_Pa> {
   File ? selected_image ;
+  String s="";
 
   Future _pickImg_Camera()async{
 
@@ -26,7 +31,26 @@ class _Temp_PaState extends State<Temp_Pa> {
       selected_image = File(returned_image!.path);
     });
   }
-
+  Future<void> uploadImage(File imageFile) async {
+    final url = Uri.parse("http://192.168.0.100:8000/upload?file");
+    final mimeTypeData = lookupMimeType(imageFile.path, headerBytes: [0xFF, 0xD8])?.split('/');
+    final imageUploadRequest = http.MultipartRequest('POST', url);
+    final file = await http.MultipartFile.fromPath(
+      'file', // এই key 'multer' এর সাথে match করতে হবে
+      imageFile.path,
+      contentType: MediaType(mimeTypeData![0], mimeTypeData[1]),
+    );
+    imageUploadRequest.files.add(file);
+    final streamedResponse = await imageUploadRequest.send();
+    final response = await http.Response.fromStream(streamedResponse);
+    if (response.statusCode == 200) {
+      print("Image uploaded successfully!");
+      print("Response body: ${response.body}");
+      s = response.body;
+    } else {
+      print("Failed to upload image: ${response.reasonPhrase}");
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,15 +59,18 @@ class _Temp_PaState extends State<Temp_Pa> {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+
             Padding(child: Image.file(selected_image!),padding: EdgeInsets.all(20)),
             ElevatedButton(
-                onPressed: (){
-                  MyApp.navigatorKey.currentState?.push(MaterialPageRoute(
-                    builder: (_) => Prescription_Analysis(),
-                  ));
-                },
-                child: Text("Submit")
+              onPressed: () async {
+                await uploadImage(selected_image!); // এই ফাংশনটি await করুন
+                MyApp.navigatorKey.currentState?.push(MaterialPageRoute(
+                  builder: (_) => Prescription_Analysis(responseData: s), // রেসপন্স পাঠানো
+                ));
+              },
+              child: Text("Submit"),
             ),
+
             ElevatedButton(
                 onPressed: (){
                   setState(() {
@@ -59,6 +86,8 @@ class _Temp_PaState extends State<Temp_Pa> {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+
+
             ElevatedButton(
               child: Text("Take a picture with camera"),
               onPressed: () {
